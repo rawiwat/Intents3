cc2ee0f8411fcc78d156508c0a8107f717de4249
Peter <rawiwatpan@gmail.com>
2022-12-14 17:18:17 +0700

    Intent to request Image

--- app/src/main/java/com/example/intents3/MainActivity.kt
+++ app/src/main/java/com/example/intents3/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.intents3
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     companion object{
         const val STRING_DATA_KEY = "PASSED_DATA"
         const val INT_DATA_KEY = "PASSED_DATA_INT"
+        const val IMAGE_REQUEST_CODE = 5
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +32,20 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra(INT_DATA_KEY, 5)
             startActivity(intent)
         }
+        intent_for_result.setOnClickListener{
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            if (intent.resolveActivity(packageManager) != null){
+            startActivityForResult(intent, IMAGE_REQUEST_CODE)
+            }
+    }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            val image = data?.data
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
 
